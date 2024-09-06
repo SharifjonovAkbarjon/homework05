cd7671276d614a8e34f1b850f19bb609d8c44f82
akbarjon <akbarjonsharifjonov33@gmail.com>
2024-09-06 08:43:08 +0500

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Pages/Home/Home'
+import Login from './components/Pages/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>Redux</h2>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="Login" element={<Login/>}/>
+  </Routes>
     </>
   )
 }
